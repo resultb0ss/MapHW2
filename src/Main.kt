b2677cd8f892808acacda6a682ab8aa779709df1
Admin <reteamboss@gmail.com>
2024-09-23 19:27:40 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("Hello World!")
+    val people = mutableMapOf(1 to "Иван", 2 to "Петр", 3 to "Лида", 4 to "Петр", 5 to "Анна")
+
+
+    println("Введите значение")
+    var input = readln()
+    delVal(input,people)
+
 }
+
+fun delVal(input: String, array: MutableMap<Int,String>){
+    println(if (input in array.values) array.filter { it.value != input } else "Такого значения нет")
+}
+
+
